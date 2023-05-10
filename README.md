27773edca2d314e736fd8e7a36846564a717c474
Mitja Felicijan <mitja.felicijan@gmail.com>
2023-05-10 12:42:13 +0200

    Engage!

---
+++ README.md
@@ -0,0 +1,6 @@
+# A bunch of tools for working with WoW
+
+## External tools
+
+- [BLP2PNG](https://www.wowinterface.com/downloads/info6127-1.2.html)
+- [MPQ Editor](https://www.hiveworkshop.com/threads/ladiks-mpq-editor.249562/)
---
+++ interface/generate.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+if [ "$#" -lt 3 ]; then
+    echo "Usage: $0 workdir outputdir toolsdir"
+    exit 1
+fi
+
+WORK_DIR="$1"
+OUTPUT_DIR="$2"
+TOOLS_DIR="$3"
+
+cp "$WORK_DIR/(listfile)" "$WORK_DIR/listfile"
+sed -i 's/\\/\//g' "$WORK_DIR/listfile" # Convert backslashes to forward slashes
+sed -i 's/\r$//' "$WORK_DIR/listfile" # Remove carriage returns
+mkdir -p "$OUTPUT_DIR/assets"
+
+echo "Work directory:   $WORK_DIR"
+echo "Output directory: $OUTPUT_DIR"
+echo "Tools directory:  $TOOLS_DIR"
+echo
+
+if [ -f "$OUTPUT_DIR/assets/listfile.txt" ]; then
+    rm -f "$OUTPUT_DIR/assets/listfile.txt"
+fi
+
+counter=1
+numassets=$(grep -c "\.blp" "$WORK_DIR/listfile")
+
+while read -r line; do
+    if [[ $line == *".blp"* ]]; then
+        namespace=$(dirname "$line")
+        filename=$(basename "$line")
+        pngfile=$(echo $line | sed 's/\.blp$/.png/')
+
+        mkdir -p "$OUTPUT_DIR/assets/$namespace"
+        wine "$TOOLS_DIR/BLP2PNG.exe" "$WORK_DIR/$line"
+        mv "$WORK_DIR/$pngfile" "$OUTPUT_DIR/assets/$namespace"
+
+        echo $line >> "$OUTPUT_DIR/assets/listfile.txt"
+        
+        echo "[$counter/$numassets]" $line
+        ((counter++))
+    fi
+done < "$WORK_DIR/listfile"
